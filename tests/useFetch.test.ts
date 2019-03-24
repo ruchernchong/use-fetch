73e63209ec29013bf2fe7a1366c360b57eb9fb2c
Ru Chern Chong <hello@ruchern.net>
2019-03-25 01:38:42 +0800

    Make test pass

--- tests/useFetch.test.ts
+++ tests/useFetch.test.ts
@@ -1,19 +1,33 @@
+import { FetchMock } from 'jest-fetch-mock';
 import { cleanup, renderHook } from 'react-hooks-testing-library';
 import useFetch from '../src/useFetch';
 
+const fetch = fetchMock as FetchMock;
+
 describe('useFetch', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
   afterEach(cleanup);
 
-  // TODO Work on the tests
   test('should fetch data', async () => {
-    // const { result, waitForNextUpdate } = renderHook(() =>
-    //   useFetch('http://jsonplaceholder.typicode.com/todos/1')
-    // );
-    //
-    // expect(result.current.isLoading).toBe(true);
-    //
-    // await waitForNextUpdate();
-    //
-    // expect(result.current.isLoading).toBe(false);
+    fetch.mockResponseOnce(JSON.stringify({ data: 12345 }));
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetch('http://jsonplaceholder.typicode.com/todos/1'),
+    );
+
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(fetch.mock.calls[0][0]).toEqual(
+      'http://jsonplaceholder.typicode.com/todos/1',
+    );
+
+    expect(result.current.isLoading).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.data).toEqual({ data: 12345 });
+    expect(result.current.isLoading).toBe(false);
   });
 });
