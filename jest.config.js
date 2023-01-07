32f8d4e5a7140f8daf3a45d23354bc42c6ebb5c4
Ru Chern Chong <hello@ruchern.xyz>
2023-01-08 01:46:00 +0800

    Clean up

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    automock: false,
-    collectCoverage: true,
-    moduleFileExtensions: ['ts', 'js'],
-    testPathIgnorePatterns: [
-        "<rootDir>/dist",
-        "<rootDir>/docs",
-        "<rootDir>/node_modules"
-    ]
+  collectCoverage: true,
+  moduleFileExtensions: ['ts', 'js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/dist',
+    '<rootDir>/docs',
+    '<rootDir>/node_modules'
+  ]
 };
